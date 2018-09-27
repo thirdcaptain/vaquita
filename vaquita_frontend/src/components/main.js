d77e887a9fbfb8ee0be415952345820d73ac8132
Sonia Chevli <soniachevli@gmail.com>
2018-09-27 10:50:43 -0700

    add welcome

---
+++ vaquita_frontend/src/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MainPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="mainPage">
+        <h1> Welcome </h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
