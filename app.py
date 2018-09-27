63701edcdc109102f3ca5d380c3301af30b5eda5
Wyrd00 <amy.tai0120@gmail.com>
2018-09-27 15:26:13 -0700

    added result in flask as variable for jinja

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def github_login():
     if account_info.ok:
         account_info_json = account_info.json()
         # return jsonify(account_info_json), 200
-        return render_template('index.html', json=account_info_json.get('repos_url'))
+        print(account_info_json)
+        return render_template('index.html', json=account_info_json.get('repos_url'), result=account_info_json)
     return "<h1>Request failed!</h1>"
 
 
